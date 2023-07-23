cd8d932d9cf134ed21bf996255bd1648ff4d779d
Sultan <ffkirito6@gmail.com>
2023-07-23 15:45:35 +0600

    added new projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,6 +3,8 @@ import './Projects.css'
 import Project1 from '../../assets/Project1.jpg'
 import Project2 from '../../assets/Project2.png'
 import Project3 from '../../assets/Project3.png'
+import Project4 from '../../assets/Project4.png'
+import Project5 from '../../assets/Project5.png'
 import Github from '../../assets/github.png'
 import Link_icon from '../../assets/link-icon.png'
 
@@ -72,6 +74,50 @@ const Projects = () => {
                     <img src={Project3} alt="Project1" />
                 </div>
             </div>
+
+
+            <div className="Project1">
+                <div className="Project1_img">
+                    <img src={Project4} alt="Project4" />
+                </div>
+                <div className="Project1_main">
+                <p>Dino Quiz</p>
+                <h2>Chrome Dino<br/><span> but with questions</span></h2>
+                <p>in this game you can play chrome dino game but with some changes like questions, if you lose you must answer to the question about js</p>
+                <div className='tech'>
+                <span>Html</span>
+                <span>Js</span>
+                </div>
+                <div className="Link">
+                <button className='source_code'>Code <a href='https://github.com/SultanKent/Game_quiz' target="_blank"
+                ><img src={Github} alt="Github_logo" /></a></button>
+                <button className='source_code'>Live Demo <a href='https://sultankent.github.io/Game_quiz/' target="_blank"
+                ><img src={Link_icon} alt="Link_icon" /></a></button>
+                </div>
+                </div>
+            </div>
+
+
+            <div className="Project1">
+                <div className="Project1_main">
+                <p>Player</p>
+                <h2>Spotify<br/><span> Music Player</span></h2>
+                <p>website is about listening to the music from spotify, if you have spotify account you should visit this website</p>
+                <div className='tech'>
+                <span>React</span>
+                <span>Api</span>
+                </div>
+                <div className="Link">
+                <button className='source_code'>Code <a href='https://github.com/SultanKent/music-player' target="_blank"
+                ><img src={Github} alt="Github_logo" /></a></button>
+                <button className='source_code'>Live Demo <a href='https://sultankent.github.io/music-player/' target="_blank"><img src={Link_icon} alt="Link_icon" /></a></button>
+                </div>
+                </div>
+                <div className="Project1_img">
+                    <img src={Project5} alt="Project5" />
+                </div>
+            </div>
+
             <p className='Github'>More in my <a href='https://github.com/SultanKent'
             target="_blank">Github</a></p>
         </div>
